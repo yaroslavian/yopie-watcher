b82cee787b8a3d981bd28f4e2bc7390baae42569
Yury Romanov <yury.romanov@bk.ru>
2018-01-26 01:54:36 +0300

    Modify syntax in datecheckerTest.

--- test/datecheckerTest.js
+++ test/datecheckerTest.js
@@ -5,22 +5,22 @@ const chai = require('chai'),
 chai.use(asserttype);
 chai.use(chaidatetime);
 
-const assert = require('chai').assert,
+const assert = chai.assert,
 	datechecker = require('../datechecker.js');
 	
 var firstRunResult = datechecker.checkDate(),
 	secondRunResult = datechecker.checkDate();
 	
-describe('datechecker test:', function() {
-	it('Should return Date', function() {
+describe('datechecker test:', () => {
+	it('Should return Date', () => {
 		assert.typeOf(firstRunResult, 'date');
 	});
 	
-	it('Date should be less than current', function() {
+	it('Date should be less than current', () => {
 		assert.beforeTime(firstRunResult, new Date());
 	});
 	
-	it('Should return null when called on the same date', function() {
+	it('Should return null when called on the same date', () => {
 		assert.equal(secondRunResult, null);
 	});
 });
